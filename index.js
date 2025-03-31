bdc818b6e4b41ab22ca1443779525803e33b2d86
ccbaron <christian7baron@gmail.com>
2025-03-31 20:43:08 +0200

    Ejercicio OK!

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // ¡Buena suerte!
+
+const btnCalcular = document.querySelector("#calculate");
+const inputBill = document.querySelector("#bill");
+const inputTip = document.querySelector("#tip");
+const spanTotal = document.querySelector("#total");
+
+function calcularTotal() {
+    const valorFactura = inputBill.value;
+    const valorPropina = inputTip.value;
+    const valorTotal = valorFactura * (1 + valorPropina / 100);
+    spanTotal.innerText = valorTotal.toFixed(2);
+    }
+btnCalcular.addEventListener("click", calcularTotal);
